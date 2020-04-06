4ee5f2e9a6f550a1ca9e4444b6921e64eb94013a
benvaro <benvaro91@gmail.com>
2020-04-06 13:42:10 +0300

    Add project files.

---
+++ WF_01/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+//ctrl + k + e  - clean code
+namespace WF_01
+{// WinForm
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region MessageBox test
+            //DialogResult dialog = MessageBox.Show("Hello!!!", "My first win form", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Asterisk);
+            //switch (dialog)
+            //{
+            //    case DialogResult.Yes:   // cw  tab tab
+            //        Console.WriteLine("You choose Yes");
+            //        break;
+            //    case DialogResult.No:
+            //        Console.WriteLine("You choose No");
+            //        break;
+            //    case DialogResult.Cancel:
+            //        Console.WriteLine("Cancel");
+            //        break;
+            //    default:
+            //        break;
+            //}
+
+            #endregion
+
+            Form form = new Form();
+            form.Text = "My form";
+            //form.Width = SystemInformation.PrimaryMonitorSize.Width / 2;
+            //form.Height= 300;
+
+            form.Size = new Size(400, 200);
+
+            //form.StartPosition = FormStartPosition.Manual;
+            //form.Top = 0;
+            //form.Left = form.Width - form.Width / 2;
+
+            form.Location = new Point(form.Size);
+            //form.BackColor = Color.Brown;  // Color - структура для вибору кольору
+            form.BackColor = Color.FromArgb(200, 156, 100);
+
+            form.Opacity = 0.75;  // коефіцієнт прозорості (від 0 до 1)
+
+            form.ShowDialog();  // показати форму із затримкою (не закривати відразу)
+        }
+    }
+}
---
+++ WF_02/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Form form = new Form();
+            form.Text = "First button";
+            form.BackColor = Color.Beige;
+
+            Button button = new Button();
+            button.Text = "Click me";
+            button.Size = new Size(100, 40);
+            button.BackColor = Color.Olive;
+            button.ForeColor = Color.Navy;
+            button.Font = new Font("Comic Sans MS", 14);
+            button.Top = form.Height / 2 - button.Height;
+            button.Left = form.Width / 2 - button.Width / 2;
+            form.Controls.Add(button); // приєднали кнопку до елементів керування форми
+
+            button.Click += Button_Click;
+            button.MouseClick += Button_MouseClick;
+            form.MouseClick += Form_MouseClick;
+            form.MouseDoubleClick += new MouseEventHandler(Form_MouseDoubleClick);
+            form.ShowDialog();
+        }
+
+        private static void Form_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            (sender as Form).Close();
+        }
+
+        private static void Form_MouseClick(object sender, MouseEventArgs e)
+        {
+            Form temp = sender as Form;
+            temp.Text = e.Button + " " + e.Location;
+
+            switch(e.Button)
+            {
+                case MouseButtons.Left:
+                    (sender as Form).BackColor = Color.Yellow;
+                    break;
+                case MouseButtons.Right:
+                    temp.BackColor = Color.LightBlue;
+                    break;
+            }
+        }
+
+        private static void Button_MouseClick(object sender, MouseEventArgs e)
+        {
+            ((sender as Button).Parent as Form).Text = $"Mouse Click {e.Location}";
+        }
+
+        static bool flag = true;
+        private static void Button_Click(object sender, EventArgs e)
+        {
+            if (flag)
+                (sender as Button).BackColor = Color.Blue;
+            else
+                (sender as Button).BackColor = Color.Olive;
+
+            flag = !flag;
+        }
+    }
+}
