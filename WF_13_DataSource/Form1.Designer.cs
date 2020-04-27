f5784e4ec6470d2e019b6a560025f1f47479c673
benvaro <benvaro91@gmail.com>
2020-04-27 14:43:12 +0300

    ListBox with dataSource

---
+++ WF_13_DataSource/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace WF_13_DataSource
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbProgramming = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbPopularity = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbText = new System.Windows.Forms.TextBox();
+            this.tbValue = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbProgramming
+            // 
+            this.lbProgramming.FormattingEnabled = true;
+            this.lbProgramming.ItemHeight = 16;
+            this.lbProgramming.Location = new System.Drawing.Point(13, 13);
+            this.lbProgramming.Name = "lbProgramming";
+            this.lbProgramming.Size = new System.Drawing.Size(220, 340);
+            this.lbProgramming.TabIndex = 0;
+            this.lbProgramming.SelectedIndexChanged += new System.EventHandler(this.lbProgramming_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnAdd);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.tbPopularity);
+            this.groupBox1.Controls.Add(this.tbName);
+            this.groupBox1.Location = new System.Drawing.Point(261, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(294, 147);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Programming Language";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.tbValue);
+            this.groupBox2.Controls.Add(this.tbText);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(261, 185);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(294, 168);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Demo SelectedValue";
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(69, 31);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(218, 22);
+            this.tbName.TabIndex = 0;
+            // 
+            // tbPopularity
+            // 
+            this.tbPopularity.Location = new System.Drawing.Point(69, 72);
+            this.tbPopularity.Name = "tbPopularity";
+            this.tbPopularity.Size = new System.Drawing.Size(218, 22);
+            this.tbPopularity.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(-3, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Popularity";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(88, 118);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(129, 23);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Text";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 17);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Value";
+            // 
+            // tbText
+            // 
+            this.tbText.Enabled = false;
+            this.tbText.Location = new System.Drawing.Point(69, 45);
+            this.tbText.Name = "tbText";
+            this.tbText.Size = new System.Drawing.Size(218, 22);
+            this.tbText.TabIndex = 1;
+            // 
+            // tbValue
+            // 
+            this.tbValue.Enabled = false;
+            this.tbValue.Location = new System.Drawing.Point(69, 100);
+            this.tbValue.Name = "tbValue";
+            this.tbValue.Size = new System.Drawing.Size(218, 22);
+            this.tbValue.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(567, 370);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lbProgramming);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbProgramming;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbPopularity;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox tbValue;
+        private System.Windows.Forms.TextBox tbText;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WF_13_DataSource/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF_13_DataSource
+{
+    public partial class Form1 : Form
+    {
+        List<ProgrammingLanguage> languages;
+        public Form1()
+        {
+            InitializeComponent();
+
+            languages = new List<ProgrammingLanguage>()
+            {
+                new ProgrammingLanguage{Name="C++", Popularity=1},
+                new ProgrammingLanguage{Name="C#", Popularity=2},
+                new ProgrammingLanguage{Name="Java", Popularity=3},
+                new ProgrammingLanguage{Name="Python", Popularity=4},
+                new ProgrammingLanguage{Name="Perl", Popularity=5},
+                new ProgrammingLanguage{Name="Objective C", Popularity=6},
+                new ProgrammingLanguage{Name="JavaScript", Popularity=7},
+                new ProgrammingLanguage{Name="Swift", Popularity=8},
+                new ProgrammingLanguage{Name="PHP", Popularity=9}
+            };
+
+            lbProgramming.DataSource = languages;
+            lbProgramming.DisplayMember = "Name";
+            lbProgramming.ValueMember = "Popularity";
+            ClearDataSource();
+        }
+
+        private void lbProgramming_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lbProgramming.SelectedIndex == -1)
+                return;
+            ProgrammingLanguage temp = lbProgramming.SelectedItem as ProgrammingLanguage;
+            tbName.Text = temp.Name;
+            tbPopularity.Text = temp.Popularity.ToString();
+
+            tbText.Text = lbProgramming.Text;
+            tbValue.Text = lbProgramming.SelectedValue.ToString();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(tbName.Text) ||
+                String.IsNullOrWhiteSpace(tbPopularity.Text))
+                return;
+            int popularity;
+            if (!int.TryParse(tbPopularity.Text, out popularity))
+                return;
+            var prog = languages.Find(x => x.Name == tbName.Text);
+            if (prog != null)
+                return;
+            ProgrammingLanguage language = new ProgrammingLanguage
+            {
+                Name = tbName.Text,
+                Popularity = popularity
+            };
+            languages.Add(language);
+
+            ClearDataSource();
+        }
+
+        private void ClearDataSource()
+        {
+            lbProgramming.DataSource = null;
+            lbProgramming.DataSource = languages;
+            lbProgramming.DisplayMember = "Name";
+            lbProgramming.ValueMember = "Popularity";
+            lbProgramming.SelectedIndex = -1;
+            lbProgramming.SelectedItem = null;
+            tbName.Text = "";
+            tbPopularity.Text = "";
+        }
+    }
+
+    public class ProgrammingLanguage   // internal - public для своїх))) видимий в межах збірки
+    {
+        public string Name { get; set; }
+        public int Popularity { get; set; }
+
+        //public override string ToString()
+        //{
+        //    return $"{Name} --- {Popularity}";
+        //}
+    }
+
+
+    #region Test base ToString()
+    class Person
+    {
+        public string Name { get; set; }
+        public override string ToString()
+        {
+            return "Name = " + Name;
+        }
+    }
+    class Student : Person
+    {
+        public int Age { get; set; }
+        public override string ToString()
+        {
+            return base.ToString() + $"\n + {Age}";
+        }
+    } 
+    #endregion
+}
