17fad81b5c082bba32142acecca5dcb21fe1bb7f
benvaro <benvaro91@gmail.com>
2020-05-11 14:30:42 +0300

    TreeView

---
+++ WF_25_TreeView/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace WF_25_TreeView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Rivne");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Lviv");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Ukraine", new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2});
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Warsaw");
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Poland", new System.Windows.Forms.TreeNode[] {
+            treeNode4});
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Country", new System.Windows.Forms.TreeNode[] {
+            treeNode3,
+            treeNode5});
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imageListNodes = new System.Windows.Forms.ImageList(this.components);
+            this.imageListCheck = new System.Windows.Forms.ImageList(this.components);
+            this.tbNode = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel4 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.contextMenuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.treeView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.treeView1.HideSelection = false;
+            this.treeView1.ImageIndex = 0;
+            this.treeView1.ImageList = this.imageListNodes;
+            this.treeView1.Indent = 30;
+            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "Node2";
+            treeNode1.Text = "Rivne";
+            treeNode2.Name = "Node3";
+            treeNode2.Text = "Lviv";
+            treeNode3.Name = "Node1";
+            treeNode3.Text = "Ukraine";
+            treeNode4.Name = "Node7";
+            treeNode4.Text = "Warsaw";
+            treeNode5.Name = "Node6";
+            treeNode5.Text = "Poland";
+            treeNode6.Name = "Node0";
+            treeNode6.Text = "Country";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode6});
+            this.treeView1.SelectedImageKey = "clock2.png";
+            this.treeView1.Size = new System.Drawing.Size(452, 403);
+            this.treeView1.StateImageList = this.imageListCheck;
+            this.treeView1.TabIndex = 0;
+            this.treeView1.BeforeLabelEdit += new System.Windows.Forms.NodeLabelEditEventHandler(this.treeView1_BeforeLabelEdit);
+            this.treeView1.AfterLabelEdit += new System.Windows.Forms.NodeLabelEditEventHandler(this.treeView1_AfterLabelEdit);
+            this.treeView1.AfterCollapse += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterCollapse);
+            this.treeView1.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView1_BeforeExpand);
+            this.treeView1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseClick);
+            this.treeView1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.treeView1_KeyDown);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clearToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(113, 28);
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(112, 24);
+            this.clearToolStripMenuItem.Text = "Clear";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.clearToolStripMenuItem_Click);
+            // 
+            // imageListNodes
+            // 
+            this.imageListNodes.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageListNodes.ImageStream")));
+            this.imageListNodes.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageListNodes.Images.SetKeyName(0, "clock.png");
+            this.imageListNodes.Images.SetKeyName(1, "clock2.png");
+            // 
+            // imageListCheck
+            // 
+            this.imageListCheck.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageListCheck.ImageStream")));
+            this.imageListCheck.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageListCheck.Images.SetKeyName(0, "transparent-green-checkmark.png");
+            this.imageListCheck.Images.SetKeyName(1, "red_unchek.jpg");
+            // 
+            // tbNode
+            // 
+            this.tbNode.Location = new System.Drawing.Point(489, 23);
+            this.tbNode.Name = "tbNode";
+            this.tbNode.Size = new System.Drawing.Size(299, 22);
+            this.tbNode.TabIndex = 1;
+            this.tbNode.Text = "Enter node name";
+            this.tbNode.Enter += new System.EventHandler(this.tbNode_Enter);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(489, 65);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(299, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add Node";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDel
+            // 
+            this.btnDel.Location = new System.Drawing.Point(489, 120);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(299, 23);
+            this.btnDel.TabIndex = 2;
+            this.btnDel.Text = "Delete Node";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2,
+            this.toolStripStatusLabel3,
+            this.toolStripStatusLabel4});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 424);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 26);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(46, 20);
+            this.toolStripStatusLabel1.Text = "Node";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(0, 20);
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(37, 20);
+            this.toolStripStatusLabel3.Text = "Path";
+            // 
+            // toolStripStatusLabel4
+            // 
+            this.toolStripStatusLabel4.Name = "toolStripStatusLabel4";
+            this.toolStripStatusLabel4.Size = new System.Drawing.Size(0, 20);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.tbNode);
+            this.Controls.Add(this.treeView1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ImageList imageListNodes;
+        private System.Windows.Forms.ImageList imageListCheck;
+        private System.Windows.Forms.TextBox tbNode;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel4;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+    }
+}
+
---
+++ WF_25_TreeView/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF_25_TreeView
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            treeView1.LabelEdit = true; // Дозволяємо редагування вузлів
+        }
+
+        private void treeView1_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            toolStripStatusLabel2.Text = "Selected: " + e.Node.Text;
+            toolStripStatusLabel4.Text = e.Node.FullPath;
+            this.Text = "Level: " + e.Node.Level;
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            treeView1.SelectedNode.Remove();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(tbNode.Text))
+                return;
+            TreeNode add = new TreeNode(tbNode.Text);
+            treeView1.SelectedNode.Nodes.Add(add);
+            tbNode.Text = "";
+        }
+
+        private void tbNode_Enter(object sender, EventArgs e)
+        {
+            tbNode.Text = "";
+        }
+
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            treeView1/*.SelectedNode*/.Nodes.Clear();
+        }
+
+        private void treeView1_BeforeLabelEdit(object sender, NodeLabelEditEventArgs e)
+        {
+            toolStripStatusLabel2.BackColor = Color.Maroon;
+            toolStripStatusLabel2.Text = "Youre going to change text of current node" + e.Node.FullPath;
+        }
+        bool isCancelled = false;
+        private void treeView1_AfterLabelEdit(object sender, NodeLabelEditEventArgs e)
+        {
+            
+            if (isCancelled)
+            {
+                toolStripStatusLabel2.Text = "U changed Text to " + e.Label;
+                toolStripStatusLabel2.BackColor = Color.Transparent;
+            }
+            else
+            {
+                toolStripStatusLabel2.Text = "U cancelled changes";
+                toolStripStatusLabel2.BackColor = Color.LightCoral;
+            }
+            
+        }
+        private void treeView1_KeyDown(object sender, KeyEventArgs e)
+        {
+            isCancelled = e.KeyCode == Keys.Escape;
+        }
+
+        //private void treeView1_KeyPress(object sender, KeyPressEventArgs e)
+        //{
+        //    isCancelled = e.KeyChar == (char)Keys.Escape;
+        //}
+
+        private void treeView1_AfterCollapse(object sender, TreeViewEventArgs e)
+        {
+            toolStripStatusLabel4.Text = "U collapsed " + e.Node;
+            //   treeView1.CollapseAll();
+            foreach (TreeNode item in treeView1.Nodes)
+            {
+                item.Collapse(); // розгорнути
+              //  item.Expand(); // згорнути
+            }
+            treeView1.Refresh();
+        }
+
+        private void treeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            toolStripStatusLabel2.Text = "Before expand " + e.Node;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+      //      treeView1_KeyDown(null, e);
+        }
+    }
+}
